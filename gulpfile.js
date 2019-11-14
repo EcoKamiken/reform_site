e18549a3117ac53261331326eef2dc5ecdae33ee
Nakajima Kazuki <nakajima.k0301@gmail.com>
2019-11-14 13:17:35 +0900

    meta情報をjsonファイルで管理するようにした。それにともないejsビルドのコマンド変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp");
 var plumber = require('gulp-plumber')
 var notify = require('gulp-notify')
-var del = require('del')
 var sass = require('gulp-sass')
 var ejs = require("gulp-ejs")
 var rename = require("gulp-rename")
+var changed = require('gulp-changed')
+var fs = require('fs')
+var del = require('del')
 var path = require('path')
 
 var destination = 'dist/reform_test'
@@ -20,19 +22,22 @@ base = [distribute, out_dir].join('/')
 // Source
 var src = {
   src: source,
-  js: [source, 'js', '*.js'].join('/'),
-  sass: [source, 'sass', '*.sass'].join('/'),
-  image: [source, 'images', '*'].join('/'),
-  service: [source, 'services', '*.json'].join('/'),
-  page: [source, 'pages', '*.ejs'].join('/'),
   component: [source, 'components', '_*.ejs'].join('/'),
+  image: [source, 'images', '*'].join('/'),
+  js: [source, 'js', '*.js'].join('/'),
+  meta: [source, 'settings', 'meta.json'].join('/'),
+  page: [source, 'pages', '*.ejs'].join('/'),
+  sass: [source, 'sass', '*.sass'].join('/'),
+  service: [source, 'services', '*.json'].join('/'),
 }
 
 // Distribute
 var dist = {
   dist: distribute,
   base: base,
-  css: [base, 'css'].join('/')
+  css: [base, 'css'].join('/'),
+  image: [base, 'images'].join('/'),
+  js: [base, 'js'].join('/'),
 }
 
 // src, distディクショナリの中身を一覧表示
@@ -45,9 +50,9 @@ gulp.task('vars', (done) => {
 })
 
 // distディレクトリを削除
-gulp.task('clean', () => {
-  console.log('Delete: ' + dist.dist)
-  return del(dist.dist)
+gulp.task('clean', (done) => {
+  del(dist.dist)
+  done()
 })
 
 // sassビルド
@@ -61,29 +66,38 @@ gulp.task('sass', (done) => {
   done()
 })
 
-gulp.task('copy', () => {
-  return gulp.src([
-      'src/images/*', 'src/js/*'
-    ], {
-      base: 'src'
-    })
-    .pipe(gulp.dest(destination))
+// imagesコピー
+gulp.task('image', (done) => {
+  gulp.src(src.image)
+    .pipe(changed(dist.image))
+    .pipe(gulp.dest(dist.image))
+  done()
 })
 
+// jsコピー
+gulp.task('js', (done) => {
+  gulp.src(src.js)
+    .pipe(changed(dist.js))
+    .pipe(gulp.dest(dist.js))
+  done()
+})
+
+// ejsビルド
 gulp.task("ejs", (done) => {
+  var meta = JSON.parse(fs.readFileSync(src.meta, 'utf8'))
   gulp
-    .src(["src/pages/*.ejs", "!" + "src/**/_*.ejs"])
-    .pipe(ejs({}, {}, {
+    .src([src.page, "!" + src.component])
+    .pipe(ejs({
+      meta
+    }, {}, {
       ext: '.html'
     }))
     .pipe(rename({
       extname: ".html"
     }))
-    .pipe(gulp.dest(destination))
+    .pipe(gulp.dest(dist.base))
   done()
 })
-
-
 
 gulp.task('watch', (done) => {
   gulp.watch('src/**/*.ejs', gulp.task('ejs'))
